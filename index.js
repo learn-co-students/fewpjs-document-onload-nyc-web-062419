10f15ef42dbc7d58a85cc6c89d1fd7d0c2ad0e6b
nicholasstano <nicholasstano@gmail.com>
2019-08-06 16:04:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("The DOM has loaded");
+  let element = document.querySelector("p#text");
+  if (element.textContent === "This is really cool!") {
+    element.textContent =
+      "JavaScript is so cool. It lets me add text to my page programmatically.";
+  } else if (
+    element.textContent ===
+    "JavaScript is so cool. It lets me add text to my page programmatically."
+  ) {
+    element.textContent = "This is really cool!";
+  }
+});
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
