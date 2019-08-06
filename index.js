b6355a26bf56a96b8cce5a339516c6c3f82089da
Flatiron School <email.the.anderson@gmail.com>
2019-08-06 16:19:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+let para = document.querySelector('p#text')
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    para.textContent = "This is really cool!"
+  });
+
+console.log("Foo")
+
